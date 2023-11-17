f85f0c0a0285b4e28e05d5c5e1a551772588c1ca
Pablo Blanco <admin@todoporaqui.com>
2023-11-17 12:06:30 -0500

    Creacion de migraciones, factories, seeders, modelos, politicas, controladores y requests

---
+++ app/Http/Controllers/MoviesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movies;
+use App\Http\Requests\StoreMoviesRequest;
+use App\Http\Requests\UpdateMoviesRequest;
+
+class MoviesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMoviesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Movies $movies)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Movies $movies)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMoviesRequest $request, Movies $movies)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Movies $movies)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TurnsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Turns;
+use App\Http\Requests\StoreTurnsRequest;
+use App\Http\Requests\UpdateTurnsRequest;
+
+class TurnsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTurnsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Turns $turns)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Turns $turns)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTurnsRequest $request, Turns $turns)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Turns $turns)
+    {
+        //
+    }
+}
